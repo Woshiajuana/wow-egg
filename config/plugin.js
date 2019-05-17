7629a829797f836a27e5d885f0fadb4f55a7eb96
Woshiajuana <zhigang.chen@owulia.com>
2019-05-17 12:48:48 +0800

    tijiao

--- config/plugin.js
+++ config/plugin.js
@@ -2,8 +2,17 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+ // had enabled by egg
+    // static: {
+    //   enable: true,
+    // }
+   nunjucks: {
+       enable: true,
+       package: 'egg-view-nunjucks',
+   }
 };
+
+// exports.nunjucks = {
+//   enable: true,
+//   package: 'egg-view-nunjucks',
+// };
