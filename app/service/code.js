f19e52ea0307c0887c0e687d43c80ddfb80a7e26
Woshiajuana <zhigang.chen@owulia.com>
2019-06-17 20:59:57 +0800

    sub code

--- app/service/code.js
+++ app/service/code.js
@@ -1,6 +1,7 @@
+'use strict';
 
-const Service = require('egg').Service;
+const { Service } = require('egg');
 
 module.exports = class CodeService extends Service {
-    
-}
+    async sendToEmail() {}
+};
