082e6fd21fdbf4b89c4aab13b4cabe9480b27497
Woshiajuana <zhigang.chen@owulia.com>
2019-06-18 16:26:01 +0800

    sub code

--- app/service/captcha.js
+++ app/service/captcha.js
@@ -4,19 +4,19 @@ const { Service } = require('egg');
 const randomString = require('randomstring');
 const ms = require('ms');
 
-module.exports = class CodeService extends Service {
+module.exports = class CaptchaService extends Service {
 
     /**
      * 根据邮件生成
      * @param   email   [String]    邮箱
      * */
     async generateByEmail(email) {
-        const { logger, app, config } = this;
+        const { logger, app, config, service } = this;
         const { redis } = app;
         const captcha = randomString.generate({ length: 6, charset: 'numeric' });
         const codeEmailMaxAge = ms(config.codeEmailMaxAge || '5m');
-        const redisKey = `captcha:${email}`;
         logger.info(`CodeService.generateByEmail=> email:${email} captcha:${captcha} codeEmailMaxAge:${codeEmailMaxAge}`);
+        const redisKey = `captcha:${email}`;
         await redis.set(redisKey, captcha, 'EX', codeEmailMaxAge * 0.001);
     }
 };
