9de5fab0f128ba4ec4a7452cb7533743155e172f
Woshiajuana <zhigang.chen@owulia.com>
2019-06-20 16:22:34 +0800

    sub code

--- app/controller/captcha.js
+++ app/controller/captcha.js
@@ -5,9 +5,11 @@ const { Controller } = require('egg');
 module.exports = class CaptchaController extends Controller {
     async sendToEmail() {
         const { ctx, service, app } = this;
-        console.log('app.validate1', app.validate1)
+        console.log('app.validate', app.validate)
+        console.log('app.validate', app.validate.check)
+        return;
         try {
-            let { email } = await app.validate1.check(ctx.request.body, (regular) => {
+            let { email } = await app.validate.check(ctx.request.body, (regular) => {
                 return {
                     email: [
                         {
