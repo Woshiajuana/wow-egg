c4470d0d38eecbf2e16b5a665e68424d0797d9e9
Woshiajuana <zhigang.chen@owulia.com>
2019-09-28 23:44:52 +0800

    subcode

--- app/router.js
+++ app/router.js
@@ -3,6 +3,7 @@
 /**
  * @param {Egg.Application} app - egg application
  */
+ 
 module.exports = app => {
     const { router, controller } = app;
     router.get('/register', controller.view.register);
